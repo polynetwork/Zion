9b1ad10493ac8d23a1bdeaa9197b639b1e013bab
dylenfu <dylenfu@126.com>
2021-08-31 11:53:09 +0800

    reset the default value of miner recommit interval

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -86,7 +86,7 @@ var Defaults = Config{
 		GasFloor: 8000000,
 		GasCeil:  8000000,
 		GasPrice: big.NewInt(params.GWei),
-		Recommit: 3 * time.Second,
+		Recommit: 1 * time.Second,
 	},
 	TxPool:      core.DefaultTxPoolConfig,
 	RPCGasCap:   25000000,
