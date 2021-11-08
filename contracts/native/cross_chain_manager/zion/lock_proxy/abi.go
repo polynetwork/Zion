3787265378a5a9092e5fe8c6b2903db5c3ddf8c0
dylenfu <dylenfu@126.com>
2021-11-08 11:50:26 +0800

    update abi

--- contracts/native/cross_chain_manager/zion/lock_proxy/abi.go
+++ contracts/native/cross_chain_manager/zion/lock_proxy/abi.go
@@ -20,6 +20,7 @@ package lock_proxy
 
 import (
 	"fmt"
+	"math/big"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -44,6 +45,7 @@ var (
 	this = utils.MainChainECCMContractAddress
 )
 
+// function name
 type MethodContractNameInput struct{}
 
 func (m *MethodContractNameInput) Encode() ([]byte, error) {
@@ -63,20 +65,49 @@ func (m *MethodContractNameOutput) Decode(payload []byte) error {
 	return utils.UnpackOutputs(ABI, MethodName, m, payload)
 }
 
-type MethodCrossChainInput struct {
-	ToChainID  uint64
-	ToContract []byte
-	Method     []byte
-	TxData     []byte
+// function bindProxy
+type MethodBindProxyInput struct {
+	ToChainId       uint64
+	TargetProxyHash []byte
 }
 
-func (i *MethodCrossChainInput) Encode() ([]byte, error) {
-	return utils.PackMethod(ABI, MethodCrossChain, i.ToChainID, i.ToContract, i.Method, i.TxData)
+func (i *MethodBindProxyInput) Encode() ([]byte, error) {
+	return utils.PackMethod(ABI, MethodBindProxyHash, i.ToChainId, i.TargetProxyHash)
 }
-func (i *MethodCrossChainInput) Decode(payload []byte) error {
-	return utils.UnpackMethod(ABI, MethodCrossChain, &i, payload)
+func (i *MethodBindProxyInput) Decode(payload []byte) error {
+	return utils.UnpackMethod(ABI, MethodBindProxyHash, i, payload)
 }
 
+// function bindAsset
+type MethodBindAssetHashInput struct {
+	FromAssetHash common.Address
+	ToChainId     uint64
+	ToAssetHash   []byte
+}
+
+func (i *MethodBindAssetHashInput) Encode() ([]byte, error) {
+	return utils.PackMethod(ABI, MethodBindAssetHash, i.FromAssetHash, i.ToChainId, i.ToAssetHash)
+}
+func (i *MethodBindAssetHashInput) Decode(payload []byte) error {
+	return utils.UnpackMethod(ABI, MethodBindAssetHash, i, payload)
+}
+
+// function lock
+type MethodLockInput struct {
+	FromAssetHash common.Address
+	ToChainId     uint64
+	ToAddress     []byte
+	Amount        *big.Int
+}
+
+func (i *MethodLockInput) Encode() ([]byte, error) {
+	return utils.PackMethod(ABI, MethodLock, i.FromAssetHash, i.ToChainId, i.ToAddress, i.Amount)
+}
+func (i *MethodLockInput) Decode(payload []byte) error {
+	return utils.UnpackMethod(ABI, MethodLock, i, payload)
+}
+
+// function verifyHeaderAndExecuteTx
 type MethodVerifyHeaderAndExecuteTxInput struct {
 	Header []byte
 	Proof  []byte
@@ -89,6 +120,49 @@ func (i *MethodVerifyHeaderAndExecuteTxInput) Decode(payload []byte) error {
 	return utils.UnpackMethod(ABI, MethodVerifyHeaderAndExecuteTx, i, payload)
 }
 
+//type MethodCrossChainInput struct {
+//	ToChainID  uint64
+//	ToContract []byte
+//	Method     []byte
+//	TxData     []byte
+//}
+//
+//func (i *MethodCrossChainInput) Encode() ([]byte, error) {
+//	return utils.PackMethod(ABI, MethodCrossChain, i.ToChainID, i.ToContract, i.Method, i.TxData)
+//}
+//func (i *MethodCrossChainInput) Decode(payload []byte) error {
+//	return utils.UnpackMethod(ABI, MethodCrossChain, &i, payload)
+//}
+
+//event BindProxyEvent(uint64 toChainId, bytes targetProxyHash);
+func emitBindProxyEvent(s *native.NativeContract, toChainID uint64, targetProxyHash []byte) error {
+	return s.AddNotify(ABI, []string{EventBindProxyEvent}, toChainID, targetProxyHash)
+}
+
+//event BindAssetEvent(address fromAssetHash, uint64 toChainId, bytes targetProxyHash, uint initialAmount);
+func emitBindAssetEvent(s *native.NativeContract,
+	fromAsset common.Address,
+	toChainID uint64,
+	targetProxyHash []byte,
+	initialAmount *big.Int) error {
+	return s.AddNotify(ABI, []string{EventBindAssetEvent}, fromAsset, toChainID, targetProxyHash, initialAmount)
+}
+
+//event LockEvent(address fromAssetHash, address fromAddress, uint64 toChainId, bytes toAssetHash, bytes toAddress, uint256 amount);
+func emitLockEvent(s *native.NativeContract,
+	fromAssetHash, fromAddress common.Address,
+	toChainID uint64,
+	toAssetHash, toAddress []byte,
+	amount *big.Int) error {
+	return s.AddNotify(ABI, []string{EventLockEvent}, fromAssetHash, fromAddress, toChainID, toAssetHash, toAddress, amount)
+}
+
+//event UnlockEvent(address toAssetHash, address toAddress, uint256 amount);
+func emitUnlockEvent(s *native.NativeContract, toAssetHash, toAddress common.Address, amount *big.Int) error {
+	return s.AddNotify(ABI, []string{EventUnlockEvent}, toAssetHash, toAddress, amount)
+}
+
+//event CrossChainEvent(address indexed sender, bytes txId, address proxyOrAssetContract, uint64 toChainId, bytes toContract, string method, bytes rawdata);
 func emitCrossChainEvent(s *native.NativeContract,
 	sender common.Address,
 	txID []byte,
@@ -101,6 +175,7 @@ func emitCrossChainEvent(s *native.NativeContract,
 	return s.AddNotify(ABI, []string{EventCrossChainEvent, sender.Hex()}, txID, proxyOrAssetContract, toChainID, toContract, method, rawData)
 }
 
+//event VerifyHeaderAndExecuteTxEvent(uint64 fromChainID, bytes toContract, bytes crossChainTxHash, bytes fromChainTxHash);
 func emitVerifyHeaderAndExecuteTxEvent(s *native.NativeContract,
 	fromChainID uint64,
 	toContract []byte,
