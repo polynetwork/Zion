21f98c64b14542b0c6ff91403b4b029987c430d4
dylenfu <dylenfu@126.com>
2021-12-02 17:20:41 +0800

    update solidity and abi

--- contracts/native/solidity/MainChainWrapper.sol
+++ contracts/native/solidity/MainChainWrapper.sol
@@ -7,11 +7,9 @@ contract MainChainWrapperTest {
     event WrapperLock(address indexed sender, address toAddress, uint64 toChainId, uint256 amount, uint256 fee);
 
     address public lockProxy;
-    address public asset;
 
     constructor() public {
-        lockProxy = address("0x7d79D936DA7833c7fe056eB450064f34A327DcA8");
-        asset = address(0);
+        lockProxy = 0x7d79D936DA7833c7fe056eB450064f34A327DcA8;
     }
 
     // user lock, `tx.Value` == `amount` + `fee`, and this value will be transfered to `wrapper` contract first, 
@@ -21,8 +19,7 @@ contract MainChainWrapperTest {
         uint256 total = amount + fee;
         require(msg.value == total, "msg.value should be equal to amount + fee!");
 
-        bytes memory toAddr = abi.encodePacked(toAddress);
-        require(IMainChainLockProxy(lockProxy).lock(asset, toChainId, toAddr, amount), "lock failed");
+        require(IMainChainLockProxy(lockProxy).lock(toChainId, toAddress, amount), "lock failed");
 
         emit WrapperLock(msg.sender, toAddress, toChainId, amount, fee);
         return true;
--- contracts/native/solidity/SideChainWrapper.sol
+++ contracts/native/solidity/SideChainWrapper.sol
@@ -5,25 +5,15 @@ import "ISideChainLockProxy.sol";
 contract SideChainWrapperTest {
 
     address public lockProxy;
-    address public asset;
     uint64 public toChainId;
 
     event WrapperBurn(address indexed sender, uint256 amount, uint256 fee);
 
     constructor() public {
         lockProxy = 0x7d79D936DA7833c7fe056eB450064f34A327DcA8;
-        asset = address(0);
         toChainId = 1;
     }
 
-    function approve(address spender, uint256 amount) external returns (bool) {
-        return ISideChainLockProxy(lockProxy).approve(spender, amount);
-    }
-
-    function allowance(address owner, address spender) external view returns (uint256) {
-        return ISideChainLockProxy(lockProxy).allowance(owner, spender);
-    }
-    
     // user burn, the `tx.value` will be transferred to `wrapper` contract first,
     // and in native contract call, the amount of `amount` will be burned, and the 
     // rest of `tx.value` will be left as handling fee.
@@ -31,7 +21,7 @@ contract SideChainWrapperTest {
         uint256 total = amount + fee;
         require(msg.value == total, "tx.value should be equal to sum of amount and fee");
         
-        require(ISideChainLockProxy(lockProxy).burn(toChainId, msg.sender, amount), "lockProxy.burn failed");
+        require(ISideChainLockProxy(lockProxy).burn(toChainId, amount), "lockProxy.burn failed");
 
         emit WrapperBurn(msg.sender, amount, fee);
         return true;
