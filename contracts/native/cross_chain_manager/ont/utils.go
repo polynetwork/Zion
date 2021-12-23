59037e5a5f9a723a2cace0061f4b33d8c25161bb
KSlashh <48985735+KSlashh@users.noreply.github.com>
2021-12-23 10:55:08 +0800

    neo n3 ont (#29)
    * neo n3 ont
    
    * rm neo

---
+++ contracts/native/cross_chain_manager/ont/utils.go
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2020 The poly network Authors
+ * This file is part of The poly network library.
+ *
+ * The  poly network  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The  poly network  is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The poly network .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ont
+
+import (
+	"fmt"
+
+	scom "github.com/ethereum/go-ethereum/contracts/native/cross_chain_manager/common"
+	otypes "github.com/ontio/ontology/core/types"
+	"github.com/polynetwork/poly/common"
+	"github.com/polynetwork/poly/merkle"
+)
+
+func VerifyFromOntTx(proof []byte, crossChainMsg *otypes.CrossChainMsg) (*scom.MakeTxParam, error) {
+	v, err := merkle.MerkleProve(proof, crossChainMsg.StatesRoot.ToArray())
+	if err != nil {
+		return nil, fmt.Errorf("VerifyFromOntTx, merkle.MerkleProve verify merkle proof error")
+	}
+
+	s := common.NewZeroCopySource(v)
+	txParam := new(scom.MakeTxParam)
+	if err := txParam.Deserialization(s); err != nil {
+		return nil, fmt.Errorf("VerifyFromOntTx, deserialize merkleValue error:%s", err)
+	}
+	return txParam, nil
+}
