2843934bc6a98d8ddfc073f8faf010cde692f4d1
dylenfu <dylenfu@126.com>
2021-12-27 17:30:20 +0800

    wrap header

--- contracts/native/helper/internal.go
+++ contracts/native/helper/internal.go
@@ -16,22 +16,44 @@
  * along with The Zion.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package eth
+package helper
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
-	scom "github.com/ethereum/go-ethereum/contracts/native/cross_chain_manager/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/internal/ethapi"
 	"github.com/ethereum/go-ethereum/light"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/trie"
 )
+
+func VerifyTx(proof []byte, hdr *types.Header, contract common.Address, extra []byte, checkResult bool) ([]byte, error) {
+	ethProof := new(ethapi.AccountResult)
+	if err := json.Unmarshal(proof, ethProof); err != nil {
+		return nil, fmt.Errorf("VerifyFromEthProof, unmarshal proof failed, err:%v", err)
+	}
+
+	proofResult, err := VerifyAccountResult(ethProof, hdr, contract)
+	if err != nil {
+		return nil, fmt.Errorf("VerifyFromEthProof, verifyMerkleProof failed, err:%v", err)
+	}
+	if proofResult == nil {
+		return nil, fmt.Errorf("VerifyFromEthProof, verifyMerkleProof failed, err:%s", "proof result is nil")
+	}
+	if checkResult && !CheckProofResult(proofResult, extra) {
+		return nil, fmt.Errorf("VerifyFromEthProof, failed to check result, stored %s, got %s", hexutil.Encode(proofResult), hexutil.Encode(extra))
+	}
+	return proofResult, nil
+}
 
 func VerifyAccountResult(proof *ethapi.AccountResult, header *types.Header, contractAddr common.Address) ([]byte, error) {
 	if err := VerifyAccountProof(proof, header, contractAddr); err != nil {
@@ -83,10 +105,10 @@ func VerifyStorageProof(proof *ethapi.AccountResult) ([]byte, error) {
 		return nil, fmt.Errorf("verifyMerkleProof, invalid storage proof format")
 	}
 	sp := proof.StorageProof[0]
-	storageKey := crypto.Keccak256(common.HexToHash(scom.Replace0x(sp.Key)).Bytes())
+	storageKey := crypto.Keccak256(common.HexToHash(Replace0x(sp.Key)).Bytes())
 
 	for _, prf := range sp.Proof {
-		nodeList.Put(nil, common.Hex2Bytes(scom.Replace0x(prf)))
+		nodeList.Put(nil, common.Hex2Bytes(Replace0x(prf)))
 	}
 	ns := nodeList.NodeSet()
 
@@ -95,4 +117,26 @@ func VerifyStorageProof(proof *ethapi.AccountResult) ([]byte, error) {
 		return nil, fmt.Errorf("verifyMerkleProof, verify storage proof error:%s\n", err)
 	}
 	return val, nil
+}
+
+func Replace0x(s string) string {
+	return strings.Replace(strings.ToLower(s), "0x", "", 1)
+}
+
+func CheckProofResult(result, value []byte) bool {
+	var s_temp []byte
+	err := rlp.DecodeBytes(result, &s_temp)
+	if err != nil {
+		log.Errorf("checkProofResult, rlp.DecodeBytes error:%s\n", err)
+		return false
+	}
+	//
+	var s []byte
+	for i := len(s_temp); i < 32; i++ {
+		s = append(s, 0)
+	}
+	s = append(s, s_temp...)
+	hash := crypto.Keccak256(value)
+
+	return bytes.Equal(s, hash)
 }
