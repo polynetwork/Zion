bab3f6444fb0e5570d66ec506a2e5bbaf44054c4
onchainzjj <zhoujunjie1031@126.com>
2022-06-30 14:07:02 +0800

    delete useless native contracts

--- contracts/native/boot/boot.go
+++ contracts/native/boot/boot.go
@@ -18,24 +18,11 @@
 package boot
 
 import (
-	"github.com/ethereum/go-ethereum/contracts/native/cross_chain_manager"
-	"github.com/ethereum/go-ethereum/contracts/native/governance"
 	"github.com/ethereum/go-ethereum/contracts/native/governance/maas_config"
-	"github.com/ethereum/go-ethereum/contracts/native/governance/neo3_state_manager"
 	"github.com/ethereum/go-ethereum/contracts/native/governance/node_manager"
-	"github.com/ethereum/go-ethereum/contracts/native/governance/relayer_manager"
-	"github.com/ethereum/go-ethereum/contracts/native/governance/side_chain_manager"
-	"github.com/ethereum/go-ethereum/contracts/native/header_sync"
 )
 
 func InitialNativeContracts() {
-	governance.InitGovernance()
-	header_sync.InitHeaderSync()
-	cross_chain_manager.InitCrossChainManager()
-	neo3_state_manager.InitNeo3StateManager()
 	node_manager.InitNodeManager()
-	relayer_manager.InitRelayerManager()
-	side_chain_manager.InitSideChainManager()
 	maas_config.InitMaasConfig()
-
 }
