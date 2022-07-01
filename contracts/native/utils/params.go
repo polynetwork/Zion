184ece6389c0a101d027cbb2ecb8ed1f4b27aa89
onchainzjj <zhoujunjie1031@126.com>
2022-07-01 10:37:29 +0800

    refactor native contracts addresses

--- contracts/native/utils/params.go
+++ contracts/native/utils/params.go
@@ -34,13 +34,8 @@ var (
 )
 
 var (
-	HeaderSyncContractAddress        = common.HexToAddress("0xb2799bDE6831449d73C1F22CE815f773D0CafCc5")
-	CrossChainManagerContractAddress = common.HexToAddress("0x5747C05FF236F8d18BB21Bc02ecc389deF853cae")
-	SideChainManagerContractAddress  = common.HexToAddress("0x864Ff06eC5fFc75aB6eaf64263308ef5fa7d6637")
-	NodeManagerContractAddress       = common.HexToAddress("0xA4Bf827047a08510722B2d62e668a72FCCFa232C")
-	RelayerManagerContractAddress    = common.HexToAddress("0xA22f301D7Cb5b50dcA4a015b12EC0cc5f3971412")
-	Neo3StateManagerContractAddress  = common.HexToAddress("0x5E839898821dB2A2F0eC9F8aAE7D7053744DB051")
-	MaasConfigContractAddress        = common.HexToAddress("0xD62B67170A6bb645f1c59601FbC6766940ee12e5")
+	NodeManagerContractAddress = common.HexToAddress("0xA4Bf827047a08510722B2d62e668a72FCCFa232C")
+	MaasConfigContractAddress  = common.HexToAddress("0xD62B67170A6bb645f1c59601FbC6766940ee12e5")
 
 	VOTE_ROUTER             = uint64(0)
 	BTC_ROUTER              = uint64(1)
