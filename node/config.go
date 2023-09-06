8da02e73b736f9a4b7ff1ff11ef0565097816b74
zhangmenghang <siovanus@126.com>
2023-09-06 21:54:47 +0800

    add p2p keystore

--- node/config.go
+++ node/config.go
@@ -23,6 +23,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"sync"
 
@@ -32,6 +33,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/scwallet"
 	"github.com/ethereum/go-ethereum/accounts/usbwallet"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/console/prompt"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p"
@@ -368,6 +370,17 @@ func (c *Config) NodeKey() *ecdsa.PrivateKey {
 	if key, err := crypto.LoadECDSA(keyfile); err == nil {
 		return key
 	}
+
+	keystoreDir := c.ResolvePath(datadirDefaultKeyStore)
+	_, err := os.Stat(keystoreDir)
+	if err == nil {
+		key, err := c.LoadKeyStore(keystoreDir)
+		if err != nil {
+			panic(fmt.Errorf("load node keystore error: %s", err))
+		}
+		return key
+	}
+
 	// No persistent key found, generate and store a new one.
 	key, err := crypto.GenerateKey()
 	if err != nil {
@@ -536,4 +549,90 @@ func (c *Config) warnOnce(w *bool, format string, args ...interface{}) {
 	}
 	l.Warn(fmt.Sprintf(format, args...))
 	*w = true
+}
+
+func (c *Config) LoadKeyStore(keystoreDir string) (*ecdsa.PrivateKey, error) {
+	scryptN := keystore.StandardScryptN
+	scryptP := keystore.StandardScryptP
+	if c.UseLightweightKDF {
+		scryptN = keystore.LightScryptN
+		scryptP = keystore.LightScryptP
+	}
+	ks := keystore.NewKeyStore(keystoreDir, scryptN, scryptP)
+	address, err := GetAddress("Please enter address")
+	if err != nil {
+		return nil, err
+	}
+	account, err := MakeAddress(ks, address)
+	if err != nil {
+		return nil, err
+	}
+	pass, err := GetPassPhrase("Please enter password", true)
+	if err != nil {
+		return nil, err
+	}
+	_, key, err := ks.GetDecryptedKey(account, pass)
+	if err != nil {
+		return nil, err
+	}
+	return key.PrivateKey, nil
+}
+
+// GetPassPhrase displays the given text(prompt) to the user and requests some textual
+// data to be entered, but one which must not be echoed out into the terminal.
+// The method returns the input provided by the user.
+func GetPassPhrase(text string, confirmation bool) (string, error) {
+	if text != "" {
+		fmt.Println(text)
+	}
+	password, err := prompt.Stdin.PromptPassword("Password: ")
+	if err != nil {
+		return "", fmt.Errorf("failed to read password: %v", err)
+	}
+	if confirmation {
+		confirm, err := prompt.Stdin.PromptPassword("Repeat password: ")
+		if err != nil {
+			return "", fmt.Errorf("failed to read password confirmation: %v", err)
+		}
+		if password != confirm {
+			return "", fmt.Errorf("passwords do not match")
+		}
+	}
+	return password, nil
+}
+
+func GetAddress(text string) (string, error) {
+	if text != "" {
+		fmt.Println(text)
+	}
+	address, err := prompt.Stdin.PromptInput("Address: ")
+	if err != nil {
+		return "", fmt.Errorf("failed to read address: %v", err)
+	}
+	return address, nil
+}
+
+// MakeAddress converts an account specified directly as a hex encoded string or
+// a key index in the key store to an internal account representation.
+func MakeAddress(ks *keystore.KeyStore, account string) (accounts.Account, error) {
+	// If the specified account is a valid address, return it
+	if common.IsHexAddress(account) {
+		return accounts.Account{Address: common.HexToAddress(account)}, nil
+	}
+	// Otherwise try to interpret the account as a keystore index
+	index, err := strconv.Atoi(account)
+	if err != nil || index < 0 {
+		return accounts.Account{}, fmt.Errorf("invalid account address or index %q", account)
+	}
+	log.Warn("-------------------------------------------------------------------")
+	log.Warn("Referring to accounts by order in the keystore folder is dangerous!")
+	log.Warn("This functionality is deprecated and will be removed in the future!")
+	log.Warn("Please use explicit addresses! (can search via `geth account list`)")
+	log.Warn("-------------------------------------------------------------------")
+
+	accs := ks.Accounts()
+	if len(accs) <= index {
+		return accounts.Account{}, fmt.Errorf("index %d higher than number of accounts %d", index, len(accs))
+	}
+	return accs[index], nil
 }
